650c4bea6ef3f63839163dc1b2c0494ec064d34d
j-uchida <j-uchida@DESKTOP-GND53B2>
2021-05-18 16:22:11 +0900

    test commit

--- SampleProject/src/SampleServlet/ServletTest.java
+++ SampleProject/src/SampleServlet/ServletTest.java
@@ -1,12 +1,6 @@
 package SampleServlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -41,39 +35,39 @@ public class ServletTest extends HttpServlet {
 		response.setContentType("text/html;charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>ExerciseServlet3</title>");
-		out.println("</head>");
-		out.println("<body>");
-
-		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/workspace?"
-				+ "serverTimezone=JST&useUnicode=true&characterEncoding=UTF-8", "root", "root")) {
-
-			String sql = "SELECT * FROM employee";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-			//Statement pStmt = conn.createStatement(sql);
-
-			//select 実行
-			ResultSet rs = pStmt.executeQuery();
-
-			while (rs.next()) {
-				//デバッグ用にMPとEXPの数値を代入
-				String name = rs.getString("employee_name");
-				out.println(name);
-			}
-
-			out.println("</body>");
-			out.println("</html>");
-
-			pStmt.close();
-		} catch (SQLException e) {
-			System.out.println("MySQLに接続できませんでした");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		PrintWriter out = response.getWriter();
+//		out.println("<!DOCTYPE html>");
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("<title>ExerciseServlet3</title>");
+//		out.println("</head>");
+//		out.println("<body>");
+//
+//		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/workspace?"
+//				+ "serverTimezone=JST&useUnicode=true&characterEncoding=UTF-8", "root", "root")) {
+//
+//			String sql = "SELECT * FROM employee";
+//			PreparedStatement pStmt = conn.prepareStatement(sql);
+//			//Statement pStmt = conn.createStatement(sql);
+//
+//			//select 実行
+//			ResultSet rs = pStmt.executeQuery();
+//
+//			while (rs.next()) {
+//				//デバッグ用にMPとEXPの数値を代入
+//				String name = rs.getString("employee_name");
+//				out.println(name);
+//			}
+//
+//			out.println("</body>");
+//			out.println("</html>");
+//
+//			pStmt.close();
+//		} catch (SQLException e) {
+//			System.out.println("MySQLに接続できませんでした");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/order-001.jsp");
 		dispatcher.forward(request, response);
